22813112c0433eeecd7c288523c68fdf547647eb
Jin Li (Wicresoft North America) <v-jnlil@microsoft.com>
2016-08-30 13:14:43 +0800

    a

---
+++ LeetCodeSolution/LeetCode.UnitTest/ProductOfArrayExceptSelfTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+
+namespace LeetCode.UnitTest
+{
+    [TestClass]
+    public class ProductOfArrayExceptSelfTest
+    {
+        [TestMethod]
+        public void TestProductOfArray()
+        {
+            int[] arr = { 1, 2, 3, 4, 5 };
+            int[] res = ProductOfArrayExcepSelf.ProductExceptSelf(arr);
+            CollectionAssert.AreEqual(res, new int[] { 120, 60, 40, 30, 24 });
+        }
+    }
+}
---
+++ LeetCodeSolution/LeetCode/ProductOfArrayExcepSelf.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class ProductOfArrayExcepSelf
+    {
+        public static int[] ProductExceptSelf(int[] nums)
+        {
+            int[] result = new int[nums.Length];
+            int right = 1;
+            result[0] = 1;
+            for (int i = 1; i < nums.Length; i++)
+                result[i] = result[i - 1] * nums[i - 1];
+
+            for(int i=nums.Length - 1; i>=0; i--)
+            {
+                result[i] *= right;
+                right *= nums[i];
+            }
+            return result;
+        }
+    }
+}
