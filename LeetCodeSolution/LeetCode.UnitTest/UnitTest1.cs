eca31eeb28b59941a8546bc029666367cdc3e7be
Jin Li (Wicresoft North America) <v-jnlil@microsoft.com>
2016-08-30 12:22:13 +0800

    Solved 167.Two Sum II

---
+++ LeetCodeSolution/LeetCode.UnitTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+
+namespace LeetCode.UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethodTwoSumII()
+        {
+            int[] arr = { 2,2,6 };
+            int[] res = TwoSumII.TwoSum(arr, 8);
+            CollectionAssert.AreEqual(res, new int[] { 1, 3 });
+        }
+    }
+}
---
+++ LeetCodeSolution/LeetCode/TwoSumII.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class TwoSumII
+    {
+        public static int[] TwoSum(int[] numbers, int target)
+        {
+            int[] result = new int[2];
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                if (numbers[i] > target)
+                    break;
+
+                int index2 = BinarySearch(numbers, i + 1, target - numbers[i]);
+
+                if (index2 != -1)
+                {
+                    result[0] = i + 1;
+                    result[1] = index2 + 1;
+                    break;
+                }
+            }
+            return result;
+        }
+
+        private static int BinarySearch(int[] numbers, int index1, int value)
+        {
+            int index2 = numbers.Length - 1;
+            while (index1 <= index2)
+            {
+                int mid = (index2 - index1) / 2 + index1;
+
+                if (value < numbers[mid])
+                    index2 = mid - 1;
+                else if (value > numbers[mid])
+                    index1 = mid + 1;
+                else
+                    return mid;
+            }
+            return -1;
+        }
+    }
+}
