8c48b1b8e7371ad967563f28ebaeb2affdfa3c4f
Jin Li (Wicresoft North America) <v-jnlil@microsoft.com>
2016-08-31 11:12:21 +0800

    Solved 287. Find the Duplicate Number

---
+++ LeetCodeSolution/LeetCode.UnitTest/FindTheDuplicateNumberTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.UnitTest
+{
+    [TestClass]
+    public class FindTheDuplicateNumberTest
+    {
+        [TestMethod]
+        public void TestMethodFindDuplicate()
+        {
+            int[] arr = { 1, 2, 2, 3, 4, 5 };
+            int result = FindTheDuplicateNumber.FindDuplicateNumber(arr);
+            Assert.AreEqual(2, result);
+        }
+    }
+}
---
+++ LeetCodeSolution/LeetCode/FindTheDuplicateNumber.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class FindTheDuplicateNumber
+    {
+        public static int FindDuplicateNumber(int [] nums)
+        {
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            for(int i =0; i<nums.Length; i++)
+            {
+                if (dic.ContainsKey(nums[i]))
+                    dic[nums[i]]++;
+                else
+                    dic[nums[i]] = 0;
+            }
+
+            foreach(KeyValuePair<int, int> i in dic)
+            {
+                if (i.Value > 0)
+                    return i.Key;
+            }
+            return -1;
+        }
+    }
+}
