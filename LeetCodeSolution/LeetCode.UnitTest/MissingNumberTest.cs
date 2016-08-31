18ee359ad05d15fa4c0b326cc9d282b87d819372
Jin Li (Wicresoft North America) <v-jnlil@microsoft.com>
2016-08-31 11:04:25 +0800

    Solved 268. Missing Number

---
+++ LeetCodeSolution/LeetCode.UnitTest/MissingNumberTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.UnitTest
+{
+    [TestClass]
+    public class MissingNumberTest
+    {
+        [TestMethod]
+        public void TestMethodFindMissingNumber()
+        {
+            int[] arr = { 0, 1, 3 };
+            int result = MissingNumber.FindMissingNumber(arr);
+            Assert.AreEqual(2, result);
+        }
+    }
+}
---
+++ LeetCodeSolution/LeetCode/MissingNumber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class MissingNumber
+    {
+        public static int FindMissingNumber(int[] nums)
+        {
+            Array.Sort(nums);
+            int left = 0, right = nums.Length;
+            while(left < right)
+            {
+                int mid = (left + right) / 2;
+                if (nums[mid] > mid)
+                    right = mid;
+                else
+                    left = mid + 1;
+            }
+            return right;
+        }
+    }
+}
