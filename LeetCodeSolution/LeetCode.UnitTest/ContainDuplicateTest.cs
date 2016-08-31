79f2f6b69d21bf83cafcc0ef4c7ac96db9a34dac
Jin Li (Wicresoft North America) <v-jnlil@microsoft.com>
2016-08-31 10:22:42 +0800

    Solved 268. Missing Number

---
+++ LeetCodeSolution/LeetCode.UnitTest/ContainDuplicateTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.UnitTest
+{
+    [TestClass]
+    public class ContainDuplicateTest
+    {
+        [TestMethod]
+        public void TestMethodIsDuplicate()
+        {
+            int[] arr = { 1, 1, 3, 4, 5, 2 };
+            bool result = ContainsDuplicate.IsDuplicate(arr);
+            Assert.AreEqual(result, true);
+        }
+    }
+}
---
+++ LeetCodeSolution/LeetCode/ContainsDuplicate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class ContainsDuplicate
+    {
+        public static bool IsDuplicate(int[] nums)
+        {
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            for(int i =0; i<nums.Length; i++)
+            {
+                if (dic.ContainsKey(nums[i]))
+                    dic[nums[i]]++;
+                else
+                    dic[nums[i]] = 0;
+            }
+
+            foreach(KeyValuePair<int, int> i in dic)
+            {
+                if (i.Value > 0)
+                    return true;
+            }
+            return false;
+        }
+    }
+}
