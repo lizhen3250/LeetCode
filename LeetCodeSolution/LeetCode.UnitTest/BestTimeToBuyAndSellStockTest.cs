50bbbad06a8f6bab645cd7eadb4129bce29fcf13
Jin Li (Wicresoft North America) <v-jnlil@microsoft.com>
2016-08-31 10:10:47 +0800

    Solved 122. Best Time To Buy and Sell Stock II

---
+++ LeetCodeSolution/LeetCode.UnitTest/BestTimeToBuyAndSellStockTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.UnitTest
+{
+    [TestClass]
+    public class BestTimeToBuyAndSellStockTest
+    {
+        [TestMethod]
+        public void TestMethodMaxProfit()
+        {
+            int[] arr = { 1, 2, 3, 5, -3, 5 };
+            int max = BestTimeToBuyAndSellStockII.MaxProfit(arr);
+            Assert.AreEqual(12, max);
+        }
+    }
+}
---
+++ LeetCodeSolution/LeetCode/BestTimeToBuyAndSellStockII.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class BestTimeToBuyAndSellStockII
+    {
+        public static int MaxProfit(int[] prices)
+        {
+            int max = 0;
+            for(int i = 1; i<prices.Length; i++)
+            {
+                int diff = prices[i] - prices[i - 1];
+                if (diff > 0)
+                    max += diff;
+            }
+            return max;
+        }
+    }
+}
