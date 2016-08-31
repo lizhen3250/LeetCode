a6a979ae9c985bfc7e64385b8263d6f23d457252
Jin Li (Wicresoft North America) <v-jnlil@microsoft.com>
2016-08-31 10:14:34 +0800

    Solved 169. Majority Element

---
+++ LeetCodeSolution/LeetCode.UnitTest/MajorityElementTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.UnitTest
+{
+    [TestClass]
+    public class MajorityElementTest
+    {
+        [TestMethod]
+        public void TestMethodFindMajorityElement()
+        {
+            int[] arr = { 1, 2, 3, 2, 5, 2, 2 };
+            int result = MajorityElement.FindMajorityElement(arr);
+            Assert.AreEqual(2, result);
+        }
+    }
+}
---
+++ LeetCodeSolution/LeetCode/MajorityElement.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class MajorityElement
+    {
+        public static int FindMajorityElement(int[] nums)
+        {
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (dic.ContainsKey(nums[i]))
+                    dic[nums[i]]++;
+                else
+                    dic[nums[i]] = 0;
+            }
+
+            foreach (KeyValuePair<int, int> pair in dic)
+            {
+                if (pair.Value >= nums.Length / 2)
+                    return pair.Key;
+            }
+
+            return -1;
+        }
+    }
+}
