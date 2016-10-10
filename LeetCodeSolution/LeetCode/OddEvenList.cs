5484c227e998fa0de129d9fb7a3721d46af48409
lizhen3250 <764482832@qq.com>
2016-10-11 00:24:23 +0800

    Solved solution 206

---
+++ LeetCodeSolution/LeetCode/OddEvenList.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class OddEvenList
+    {
+        public ListNode OddEvenLinkedList(ListNode head)
+        {
+            if (head == null)
+                return head;
+
+            ListNode oddHead = head, evenHead = head.next;
+            ListNode prevOdd = oddHead, prevEven = evenHead;
+
+            while(prevOdd.next != null && prevEven.next != null)
+            {
+                prevOdd.next = prevEven.next;
+                prevOdd = prevOdd.next;
+
+                prevEven.next = prevOdd.next;
+                prevEven = prevEven.next;
+            }
+            prevOdd.next = evenHead;
+
+            return oddHead;
+        }
+    }
+}
