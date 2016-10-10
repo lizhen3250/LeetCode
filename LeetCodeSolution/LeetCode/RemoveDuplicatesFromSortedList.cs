9268056f811bc5966d8b6a749d336b970e100dc9
lizhen3250 <764482832@qq.com>
2016-10-11 00:33:14 +0800

    solved solution 83

---
+++ LeetCodeSolution/LeetCode/RemoveDuplicatesFromSortedList.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class RemoveDuplicatesFromSortedList
+    {
+        public ListNode DeleteDuplicates(ListNode head)
+        {
+            if (head == null || head.next == null)
+                return head;
+
+            ListNode current = head, after = head.next;
+
+            while(after != null)
+            {
+                if (current.val == after.val)
+                {
+                    current.next = after.next;
+                    after = after.next;
+                }
+                else
+                {
+                    current = after;
+                    after = after.next;
+                }
+            }
+            return head;
+        }
+    }
+}
