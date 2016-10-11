cb6a53661306a9818b4da4df9a94079ca55b15d8
lizhen3250 <764482832@qq.com>
2016-10-11 18:09:09 +0800

    Solved Merge two sorted list

---
+++ LeetCodeSolution/LeetCode/MergeTwoSortedList.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class MergeTwoSortedList
+    {
+        public ListNode MergeTwoLists(ListNode l1, ListNode l2)
+        {
+            if (l1 == null) return l2;
+            if (l2 == null) return l1;
+
+            if(l1.val < l2.val)
+            {
+                ListNode temp = l1;
+                temp.next = MergeTwoLists(l1.next, l2);
+                return temp;
+            }
+            else
+            {
+                ListNode temp = l2;
+                temp.next = MergeTwoLists(l1, l2.next);
+                return temp;
+            }
+        }
+    }
+}
