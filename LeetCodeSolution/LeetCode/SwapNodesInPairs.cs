32f60d1be5c9d67de12c6c7acd55b913ec6819c8
lizhen3250 <764482832@qq.com>
2016-10-11 18:13:30 +0800

    Solved Swap Nodes in pairs

---
+++ LeetCodeSolution/LeetCode/SwapNodesInPairs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class SwapNodesInPairs
+    {
+        public ListNode SwapPairs(ListNode head)
+        {
+            if (head == null || head.next == null)
+                return head;
+
+            ListNode temp = head.next;
+            head.next = SwapPairs(temp.next);
+            temp.next = head;
+            return temp;
+        }
+    }
+}
