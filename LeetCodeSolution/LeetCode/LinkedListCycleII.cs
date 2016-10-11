1abd6333c8a4d010defb0798eb57fd44aaa5dcb8
lizhen3250 <764482832@qq.com>
2016-10-11 18:53:34 +0800

    Sovled Linked List Cycle II

---
+++ LeetCodeSolution/LeetCode/LinkedListCycleII.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class LinkedListCycleII
+    {
+        public ListNode DetectCycle(ListNode head)
+        {
+            HashSet<ListNode> visited = new HashSet<ListNode>();
+            while(head != null)
+            {
+                if (visited.Contains(head))
+                    return head;
+                else
+                    visited.Add(head);
+
+                head = head.next;
+            }
+
+            return null;
+        }
+    }
+}
