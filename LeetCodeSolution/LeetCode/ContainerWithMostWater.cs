d9b1bde13ba7579679c9e74d48b0730d3c5c952e
lizhen3250 <764482832@qq.com>
2016-11-02 13:58:59 +0800

    Solved Container with most water

---
+++ LeetCodeSolution/LeetCode/ContainerWithMostWater.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class ContainerWithMostWater
+    {
+        public int MaxArea(int[] height)
+        {
+            int max = 0, i = 0, area = 0, j = height.Length - 1;
+
+            while(i != j)
+            {
+                if(height[i] < height[j])
+                {
+                    area = height[i] * (j - i);
+                    ++i;
+                }
+                else
+                {
+                    area = height[j] * (j - i);
+                    --j;
+                }
+                max = Math.Max(max, area);
+            }
+            return max;
+        }
+    }
+}
